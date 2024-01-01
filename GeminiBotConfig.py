16e2c58f99d7838a20ac8b3bc4e170de21c3a5dd
INBORA <96738915+InboraStudio@users.noreply.github.com>
2024-01-01 10:31:11 +0530

    Update GeminiBotConfig.py

--- GeminiBotConfig.py
+++ GeminiBotConfig.py
@@ -1,6 +1,6 @@
 #Create at https://makersuite.google.com/
-GOOGLE_AI_KEY = "AI Token here"
+GOOGLE_AI_KEY = "AIzaSyB3aB87MB3uZ0V-hYbADhI7IiBBvs_PgAs"
 #Create at https://discord.com/developers/applications
-DISCORD_BOT_TOKEN = "Discord bot Token here"
+DISCORD_BOT_TOKEN = "MTE5MTEzMzA5MTk2MDQ2MzM2MA.GmbOoR.BkyiSWrl20k0TMNMK94V3CXGaumvpIANh3Rpsk"
 #Disable history by setting to 0
 MAX_HISTORY = 15
