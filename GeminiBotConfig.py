4e53d1c7b50b52b6635eb3b098dcf413e87b141a
INBORA <96738915+InboraStudio@users.noreply.github.com>
2024-01-01 10:30:08 +0530

    Add files via upload

---
+++ GeminiBotConfig.py
@@ -0,0 +1,6 @@
+#Create at https://makersuite.google.com/
+GOOGLE_AI_KEY = "AI Token here"
+#Create at https://discord.com/developers/applications
+DISCORD_BOT_TOKEN = "Discord bot Token here"
+#Disable history by setting to 0
+MAX_HISTORY = 15
